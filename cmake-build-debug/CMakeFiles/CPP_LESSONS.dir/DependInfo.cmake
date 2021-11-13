460e2f9531d7f318c64b2ea3db2010863e6e1909
Tchannikov Daniel <75910357+Daniel-Developer-p@users.noreply.github.com>
2021-11-13 19:23:10 +0300

    bruh

---
+++ cmake-build-debug/CMakeFiles/CPP_LESSONS.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/daniiltcannikov/CLionProjects/CPP_LESSONS/06112021.cpp" "/Users/daniiltcannikov/CLionProjects/CPP_LESSONS/cmake-build-debug/CMakeFiles/CPP_LESSONS.dir/06112021.cpp.o"
+  "/Users/daniiltcannikov/CLionProjects/CPP_LESSONS/Figure.cpp" "/Users/daniiltcannikov/CLionProjects/CPP_LESSONS/cmake-build-debug/CMakeFiles/CPP_LESSONS.dir/Figure.cpp.o"
+  "/Users/daniiltcannikov/CLionProjects/CPP_LESSONS/Fraction.cpp" "/Users/daniiltcannikov/CLionProjects/CPP_LESSONS/cmake-build-debug/CMakeFiles/CPP_LESSONS.dir/Fraction.cpp.o"
+  "/Users/daniiltcannikov/CLionProjects/CPP_LESSONS/FractionAdder.cpp" "/Users/daniiltcannikov/CLionProjects/CPP_LESSONS/cmake-build-debug/CMakeFiles/CPP_LESSONS.dir/FractionAdder.cpp.o"
+  "/Users/daniiltcannikov/CLionProjects/CPP_LESSONS/Rectangle.cpp" "/Users/daniiltcannikov/CLionProjects/CPP_LESSONS/cmake-build-debug/CMakeFiles/CPP_LESSONS.dir/Rectangle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CPP_LESSONS.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_LESSONS.dir/06112021.cpp.o"
+  "CMakeFiles/CPP_LESSONS.dir/Figure.cpp.o"
+  "CMakeFiles/CPP_LESSONS.dir/Fraction.cpp.o"
+  "CMakeFiles/CPP_LESSONS.dir/FractionAdder.cpp.o"
+  "CMakeFiles/CPP_LESSONS.dir/Rectangle.cpp.o"
+  "CPP_LESSONS"
+  "CPP_LESSONS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_LESSONS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
