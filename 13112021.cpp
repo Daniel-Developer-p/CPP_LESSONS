7a195915896d8a575df9c37b27c066dde7b09ae4
Tchannikov Daniel <75910357+Daniel-Developer-p@users.noreply.github.com>
2021-11-13 20:09:28 +0300

    Update this file

--- 13112021.cpp
+++ 13112021.cpp
@@ -3,21 +3,17 @@
 class D {
 public:
     virtual void display() {
-        std::cout << "A" << std::endl;
+        std::cout << "D" << std::endl;
     }
 };
 
 class C : public D {
-public:
-    virtual void display(int i) {
-        std::cout << "A" << std::endl;
-    }
 };
 
 class B : public C {
 public:
-    virtual void display(int i) {
-        std::cout << "A" << std::endl;
+    virtual void display() {
+        std::cout << "B" << std::endl;
     }
 };
 
@@ -29,16 +25,18 @@ public:
 };
 
 int main() {
-    A a;
-    B b;
-    C c;
     D d;
-
-    D& linkToA = a;
-    linkToA.display();
-
-    a.display(1);
-    b.display(2);
-    c.display(3);
+    C c;
+    B b;
     d.display();
+    c.display();
+    b.display();
+
+    /*
+    D& linkToA = c;
+    b.display();
+    c.display();
+    d.display();
+    linkToA.display();
+    */
 }
